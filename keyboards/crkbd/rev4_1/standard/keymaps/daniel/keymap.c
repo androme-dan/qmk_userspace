66d903692f42250549957756778b34c2ef428f8e
Daniel Wieser <>
2025-03-27 13:53:19 +0100

    Add symbol layer.

--- keyboards/crkbd/rev4_1/standard/keymaps/daniel/keymap.c
+++ keyboards/crkbd/rev4_1/standard/keymaps/daniel/keymap.c
@@ -20,10 +20,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 enum layer_names {
     _COLEMAK,
     _NUMBER,
+    _SYMBOL,
 };
 
 // Layer keys
 #define LT_NUM LT(_NUMBER, KC_SPC)
+#define LT_SYM LT(_SYMBOL, KC_TAB)
 
 // Left-hand home row mods
 #define HOME_A LGUI_T(KC_A)
@@ -42,14 +44,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    QK_BOOT,     QK_BOOT,    KC_J,     KC_L,    KC_U,     KC_Y,    KC_QUOTE,  _______,
         KC_RALT,  HOME_A,  HOME_R,  HOME_S,  HOME_T,  KC_G,    QK_REBOOT,   QK_REBOOT,  KC_M,     HOME_N,  HOME_E,   HOME_I,  HOME_O,    KC_RALT,
         _______,  KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                             KC_K,     KC_H,    KC_COMM,  KC_DOT,  KC_SLSH,   _______,
-                                             KC_ESC,  LT_NUM,  KC_TAB,      KC_ENT,     KC_BSPC,  KC_DEL
+                                             KC_ESC,  LT_NUM,  LT_SYM,      KC_ENT,     KC_BSPC,  KC_DEL
 
   ),
     [_NUMBER] = LAYOUT_split_3x6_3_ex2(
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,        _______,  KC_LBRC,  KC_7,     KC_8,  KC_9,  KC_RBRC,  _______,
-        KC_RALT,  KC_LGUI,  KC_LALT,  KC_LCTL,  KC_LSFT,  _______,  _______,        _______,  KC_SCLN,  KC_4,     KC_5,  KC_6,  KC_EQL,   _______,
-        _______,  _______,  _______,  _______,  _______,  _______,                            KC_GRV,   KC_1,     KC_2,  KC_3,  KC_SLSH,  _______,
-                                                _______,  _______,  _______,        KC_DOT,   KC_0,     KC_MINS
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,        _______,  KC_LBRC,  KC_7,   KC_8,  KC_9,  KC_RBRC,  _______,
+        KC_RALT,  KC_LGUI,  KC_LALT,  KC_LCTL,  KC_LSFT,  _______,  _______,        _______,  KC_SCLN,  KC_4,   KC_5,  KC_6,  KC_EQL,   _______,
+        _______,  _______,  _______,  _______,  _______,  _______,                            KC_GRV,   KC_1,   KC_2,  KC_3,  KC_SLSH,  _______,
+                                                _______,  _______,  _______,        KC_MINS,  KC_0,     KC_DOT
+
+  ),
+    [_SYMBOL] = LAYOUT_split_3x6_3_ex2(
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,        _______,  KC_LCBR,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RCBR,  _______,
+        KC_RALT,  KC_LGUI,  KC_LALT,  KC_LCTL,  KC_LSFT,  _______,  _______,        _______,  KC_COLN,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_PLUS,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,                            KC_TILD,  KC_EXLM,  KC_AT,    KC_HASH,  KC_PIPE,  _______,
+                                                _______,  _______,  _______,        KC_UNDS,  KC_LPRN,  KC_RPRN
 
   )
 };

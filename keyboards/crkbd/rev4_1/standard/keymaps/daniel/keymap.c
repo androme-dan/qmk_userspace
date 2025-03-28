59f578d49e15e4bad8f121ab84cc86a19437fc52
Frank Ebel <git@ebel.page>
2025-03-28 18:20:00 +0100

    style: align columns

--- keyboards/crkbd/rev4_1/standard/keymaps/daniel/keymap.c
+++ keyboards/crkbd/rev4_1/standard/keymaps/daniel/keymap.c
@@ -30,16 +30,16 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK] = LAYOUT_split_3x6_3_ex2(
-        _______,   KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,   _______,     _______,  KC_J,    KC_L,    KC_U,     KC_Y,    KC_QUOTE,  _______,
-        KC_RALT,  HOME_A,  HOME_R,  HOME_S,  HOME_T,  KC_G,    _______,     _______,  KC_M,    HOME_N,  HOME_E,   HOME_I,  HOME_O,    KC_RALT,
-        _______,  KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                           KC_K,    KC_H,    KC_COMM,  KC_DOT,  KC_SLSH,   _______,
-                                             LT_FUN,  LT_NUM,  LT_SYM,      KC_ENT,   LT_NAV,  LT_MED
+        _______,  KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,     _______,        _______,  KC_J,     KC_L,     KC_U,     KC_Y,     KC_QUOTE, _______,
+        KC_RALT,  HOME_A,   HOME_R,   HOME_S,   HOME_T,   KC_G,     _______,        _______,  KC_M,     HOME_N,   HOME_E,   HOME_I,   HOME_O,   KC_RALT,
+        _______,  KC_Z,     KC_X,     KC_C,     KC_D,     KC_V,                               KC_K,     KC_H,     KC_COMM,  KC_DOT,   KC_SLSH,  _______,
+                                                LT_FUN,   LT_NUM,   LT_SYM,         KC_ENT,   LT_NAV,   LT_MED
 
   ),
     [_NUMBER] = LAYOUT_split_3x6_3_ex2(
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,        _______,  KC_LBRC,  KC_7,   KC_8,  KC_9,  KC_RBRC,  _______,
-        KC_RALT,  KC_LGUI,  KC_LALT,  KC_LCTL,  KC_LSFT,  _______,  _______,        _______,  KC_SCLN,  KC_4,   KC_5,  KC_6,  KC_EQL,   _______,
-        _______,  _______,  _______,  _______,  _______,  _______,                            KC_GRV,   KC_1,   KC_2,  KC_3,  KC_SLSH,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,        _______,  KC_LBRC,  KC_7,     KC_8,     KC_9,     KC_RBRC,  _______,
+        KC_RALT,  KC_LGUI,  KC_LALT,  KC_LCTL,  KC_LSFT,  _______,  _______,        _______,  KC_SCLN,  KC_4,     KC_5,     KC_6,     KC_EQL,   _______,
+        _______,  _______,  _______,  _______,  _______,  _______,                            KC_GRV,   KC_1,     KC_2,     KC_3,     KC_SLSH,  _______,
                                                 _______,  _______,  _______,        KC_MINS,  KC_0,     KC_DOT
 
   ),
@@ -51,9 +51,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   ),
     [_FUNCTION] = LAYOUT_split_3x6_3_ex2(
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,        _______,  KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_PSCR,  KC_PWR,
-        KC_RALT,  KC_LGUI,  KC_LALT,  KC_LCTL,  KC_LSFT,  _______,  _______,        _______,  KC_F5,  KC_F6,   KC_F7,   KC_F8,   KC_SCRL,  KC_SLEP,
-        _______,  _______,  _______,  _______,  _______,  _______,                            KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_PAUS,  KC_WAKE,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,        _______,  KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_PWR,
+        KC_RALT,  KC_LGUI,  KC_LALT,  KC_LCTL,  KC_LSFT,  _______,  _______,        _______,  KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_SCRL,  KC_SLEP,
+        _______,  _______,  _______,  _______,  _______,  _______,                            KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_PAUS,  KC_WAKE,
                                                 _______,  _______,  _______,        _______,  _______,  _______
 
   ),
